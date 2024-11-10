b8f3c1cacd2e2bfa761aeb4dbee414985b5e19d1
Brian Huynh <bah36@calvin.edu>
2024-11-10 16:56:37 -0500

    changed sql

--- server.js
+++ server.js
@@ -21,66 +21,66 @@ const router = express.Router();
 router.use(express.json());
 
 router.get('/', readHelloMessage);
-router.get('/users', readUsers);
-router.get('/events', readEvents);
-router.post('/users', createUser);
-router.post('/events', createEvent);
+// router.get('/users', readUsers);
+// router.get('/events', readEvents);
+// router.post('/users', createUser);
+// router.post('/events', createEvent);
 
 app.use(router);
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // Implement the CRUD operations.
 
-function returnDataOr404(res, data) {
-  if (data == null) {
-    res.sendStatus(404);
-  } else {
-    res.send(data);
-  }
-}
+// function returnDataOr404(res, data) {
+//   if (data == null) {
+//     res.sendStatus(404);
+//   } else {
+//     res.send(data);
+//   }
+// }
 
 function readHelloMessage(req, res) {
   res.send('Hello, CS 262 funteam service!');
 }
 
-function readUsers(req, res, next) {
-  db.many('SELECT * FROM User')
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      next(err);
-    });
-}
+// function readUsers(req, res, next) {
+//   db.many('SELECT * FROM User')
+//     .then((data) => {
+//       returnDataOr404(res, data);
+//     })
+//     .catch((err) => {
+//       next(err);
+//     });
+// }
 
-function readEvents(req, res, next) {
-  db.many('SELECT * FROM Events')
-    .then((data) => {
-      returnDataOr404(res, data);
-    })
-    .catch((err) => {
-      next(err);
-    });
-}
+// function readEvents(req, res, next) {
+//   db.many('SELECT * FROM Events')
+//     .then((data) => {
+//       returnDataOr404(res, data);
+//     })
+//     .catch((err) => {
+//       next(err);
+//     });
+// }
 
-  function createUser(req, res, next) {
-    const { username, password, name, eventID } = req.body;
-    db.none('INSERT INTO User(username, password, name, eventID) VALUES(${username}, ${password}, ${name}, ${eventID})', req.body)
-      .then((data) => {
-        res.send(data);
-      })
-      .catch((err) => {
-        next(err);
-      });
-  }
+//   function createUser(req, res, next) {
+//     const { username, password, name, eventID } = req.body;
+//     db.none('INSERT INTO User(username, password, name, eventID) VALUES(${username}, ${password}, ${name}, ${eventID})', req.body)
+//       .then((data) => {
+//         res.send(data);
+//       })
+//       .catch((err) => {
+//         next(err);
+//       });
+//   }
 
-  function createEvent(req, res, next) {
-    const { name, location, date, time, description, organizerID, tagsID } = req.body;
-    db.none('INSERT INTO Events(name, location, date, time, description, organizerID, tagsID) VALUES(${name}, ${location}, ${date}, ${time}, ${description}, ${organizerID}, ${tagsID})', req.body)
-      .then(() => {
-        res.send(data);
-      })
-      .catch((err) => {
-        next(err);
-      });
-  }
+//   function createEvent(req, res, next) {
+//     const { name, location, date, time, description, organizerID, tagsID } = req.body;
+//     db.none('INSERT INTO Events(name, location, date, time, description, organizerID, tagsID) VALUES(${name}, ${location}, ${date}, ${time}, ${description}, ${organizerID}, ${tagsID})', req.body)
+//       .then(() => {
+//         res.send(data);
+//       })
+//       .catch((err) => {
+//         next(err);
+//       });
+//   }
--- sql/eventSphere.sql
+++ sql/eventSphere.sql
@@ -8,7 +8,6 @@ CREATE TABLE Account (
     Accountname VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
-    eventID INTEGER
 );
 
 CREATE TABLE Tags (
@@ -24,17 +23,16 @@ CREATE TABLE Events (
     date DATE NOT NULL,
     time TIME NOT NULL,
     description TEXT NOT NULL,
-    organizerID INTEGER REFERENCES Account(ID),
-    tagsID INTEGER REFERENCES Tags(ID)
+    organizerID INTEGER REFERENCES Account(ID)
 );
 
 CREATE TABLE TagsList (
     ID integer PRIMARY KEY,
-    eventID INTEGER REFERENCES Events(ID),
+    eventID INTEGER REFERENCES Events(ID)
     tagID INTEGER REFERENCES Tags(ID)
 );
 
--- Allow users to seelct data from the tables
+-- Allow users to select data from the tables
 GRANT SELECT ON Account TO public;
 GRANT SELECT ON Events TO public;
 GRANT SELECT ON Tags TO public;
@@ -48,15 +46,15 @@ INSERT INTO Tags VALUES(4, 'red', 'Academic');
 INSERT INTO Tags VALUES(5, 'purple', 'Workshop');
 
 -- Insert Accounts
-INSERT INTO Account VALUES(1, 'Account1', 'password1', 'Account 1', 1);
-INSERT INTO Account VALUES(2, 'Account2', 'password2', 'Account 2', 2);
-INSERT INTO Account VALUES(3, 'Account3', 'password3', 'Account 3', 3);
+INSERT INTO Account VALUES(1, 'Account1', 'password1', 'name 1');
+INSERT INTO Account VALUES(2, 'Account2', 'password2', 'name 2');
+INSERT INTO Account VALUES(3, 'Account3', 'password3', 'name 3');
 
 -- Insert Events
-INSERT INTO Events VALUES(1, 'Sample Event 1', 'Location 1', '2021-01-01', '12:00:00', 'Description for Sample Event 1', 1, 1);
-INSERT INTO events VALUES(2, 'Sample Event 2', 'Location 2', '2021-01-02', '1:00:00', 'Description for Sample Event 2', 2, 2);
-INSERT INTO Events VALUES(3, 'Sample Event 3', 'Location 3', '2021-01-03', '2:00:00', 'Description for Sample Event 3', 3, 3);
-INSERT INTO Events VALUES(4, 'Sample Event 4', 'Location 4', '2021-01-04', '3:00:00', 'Description for Sample Event 4', 3, 4);
+INSERT INTO Events VALUES(1, 'Sample Event 1', 'Location 1', '2021-01-01', '12:00:00', 'Description for Sample Event 1', 1);
+INSERT INTO events VALUES(2, 'Sample Event 2', 'Location 2', '2021-01-02', '1:00:00', 'Description for Sample Event 2', 2);
+INSERT INTO Events VALUES(3, 'Sample Event 3', 'Location 3', '2021-01-03', '2:00:00', 'Description for Sample Event 3', 3);
+INSERT INTO Events VALUES(4, 'Sample Event 4', 'Location 4', '2021-01-04', '3:00:00', 'Description for Sample Event 4', 3);
 
 -- Assign tags to events
 INSERT INTO TagsList VALUES(1, 1, 1); -- assign tag 1 to event 1
