2572422f580fc6fea22323c300aa36305e97004c
Chaerry22 <dchae22@gmail.com>
2024-11-13 13:45:09 -0500

    more routes

--- server.js
+++ server.js
@@ -16,15 +16,22 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
 app.get('/', (req, res) => res.send('Hello, CS 262 funteam service!'));
-app.get('/users', readUsers);
-app.get('/events', readEvents); // Simple SELECT *
-app.get('/events/formatted', readEventsWithTags); // Events with nested tags
-add.get('/events/:id/tags', readEventTags);
+
+//Read/ Get
+app.get('/users', readUsers);               // Retrieve all users
+app.get('/events', readEvents);             // Retrieve all events
+app.get('/events/:id/tags', readEventTags); // Retrieve tags for a single event by ID
+app.get('/tags', readTags);                 // Retrieve all predefined tags
+app.get('/events/:id', readEvent);          // Retrieve a single event by ID
+
+//Create/ Post
 app.post('/users', createUser);
 app.post('/events', createEvent);
 
+// Update/ Put
+
+// Delete
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 function returnDataOr404(res, data) {
@@ -48,6 +55,24 @@ function readUsers(req, res, next) {
     .then((data) => returnDataOr404(res, data))
     .catch(next);
 }
+
+// Retrieve tags
+function readTags(req, res, next) {
+  db.manyOrNone('SELECT * FROM Tags')
+    .then((data) => returnDataOr404(res, data))
+    .catch(next);
+}
+
+function readEvent(req, res, next) {
+  db.oneOrNone('SELECT * FROM Events WHERE id=${id}', req.params)
+    .then((data) => {
+      returnDataOr404(res, data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+}
+
 
 // Retrieve tags for event id 
 function readEventTags(req, res, next) {
